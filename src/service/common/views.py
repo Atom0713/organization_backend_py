9d256a6cfbede4cbd9396f7bb47bfebf1625ff37
Artem Sliusarenko <sliusarenko.artem@gmail.com>
2022-09-07 22:55:06 +0200

    health check respond to all clients

--- src/service/common/views.py
+++ src/service/common/views.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, jsonify
+from flask_cors import cross_origin
 
 bp = Blueprint("common", __name__)
 
 
 @bp.route("/", methods=["GET"])
+@cross_origin()
 def health_check():
     return jsonify({"status": "ok"})
