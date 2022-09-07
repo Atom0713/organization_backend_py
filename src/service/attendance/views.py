6dbe881c0524c94193024f5c513c4cc39ac8de88
Artem Sliusarenko <sliusarenko.artem@gmail.com>
2022-09-07 21:58:30 +0200

    core CLIENT_URL env variable

--- src/service/attendance/views.py
+++ src/service/attendance/views.py
@@ -1,8 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from .controller import resolve_post_attendance
+from ..constants import HTTPRequestMethods
 
 bp = Blueprint("attendance", __name__, url_prefix="/attendance")
 
 @bp.route("/", methods=['GET', "POST"])
 def handle_attendance():
-    return resolve_post_attendance()
+    if request.method == HTTPRequestMethods.POST: 
+        return resolve_post_attendance()
