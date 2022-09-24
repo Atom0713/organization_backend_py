652bd7488c13eafe8e8b87ab3ecdb18be315b622
Artem Sliusarenko <sliusarenko.artem@gmail.com>
2022-09-24 23:08:35 +0200

    apply black and isort

--- src/service/events/views.py
+++ src/service/events/views.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, request
 
 from ..constants import HTTPRequestMethods
-from .controller import (resolve_get_event, resolve_get_event_attendance, resolve_post_event_attendance,
-                         resolve_get_events, resolve_post_events)
+from .controller import (resolve_get_event, resolve_get_event_attendance,
+                         resolve_get_events, resolve_post_event_attendance,
+                         resolve_post_events)
 
 bp = Blueprint("events", __name__, url_prefix="/event")
 
