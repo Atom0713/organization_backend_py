876198c3625699342fde90cb61403d0fc3da93d8
Artem Sliusarenko <sliusarenko.artem@gmail.com>
2022-10-04 14:04:31 +0200

    encode/decode auth token

---
+++ src/tests/unit/auth/test_token.py
@@ -0,0 +1,16 @@
+from src.service.auth.token import encode_auth_token, decode_auth_token
+
+import pytest
+
+
+@pytest.mark.parametrize("user_id", [1])
+def test_encode_auth_token(user_id):
+    auth_token = encode_auth_token(user_id)
+    assert isinstance(auth_token, str)
+
+
+@pytest.mark.parametrize("expected_user_id", [1])
+def test_dencode_auth_token(expected_user_id):
+    auth_token = encode_auth_token(expected_user_id)
+    assert isinstance(auth_token, str)
+    assert decode_auth_token(auth_token) == expected_user_id
