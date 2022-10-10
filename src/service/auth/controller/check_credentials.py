af9decb246b85f175eb4e4a4975f3b570fbf62fd
Artem Sliusarenko <sliusarenko.artem@gmail.com>
2022-10-10 10:16:29 +0200

    login unique user

--- src/service/auth/controller/check_credentials.py
+++ src/service/auth/controller/check_credentials.py
@@ -7,5 +7,11 @@ def check_credentials() -> Optional[int]:
     username = request.json.get("username", None)
     password = request.json.get("password", None)
     user = get_user_by_username_password(username, password)
-    return user.id if user else None
+
+    if not user:
+        return None
+    if len(user) > 1:
+        pass # TODO abort
+
+    return user[0].id
     
