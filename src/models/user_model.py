8d6a3400b07771a1137d3c631f87f55937d167f3
Artem Sliusarenko <sliusarenko.artem@gmail.com>
2022-10-10 10:15:02 +0200

    user model add last_name attribute

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -4,6 +4,7 @@ from src.datastore import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(50), nullable=False)
     dob = db.Column(db.DateTime, nullable=False)
