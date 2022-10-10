d3795f0a1fe6da8effbc2d7d3837496a19a970eb
Artem Sliusarenko <sliusarenko.artem@gmail.com>
2022-10-10 09:05:01 +0200

    MySQL database via SQLalchemy

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from src import create_app
+from src.datastore import db
 
 app = create_app()
+db.init_app(app)
+# with app.app_context():
+#     db.create_all()
 
 if __name__ == "__main__":
     app.run()
