4b2213b64912912beb72f4e8e10ba1c18b06c357
Artem Sliusarenko <sliusarenko.artem@gmail.com>
2023-01-03 08:37:02 +0100

    player positions model

--- src/models/position.py
+++ src/models/position.py
@@ -1,14 +1,11 @@
+from typing import Dict
+
 from src.datastore import db
 
 
-class Role(db.Model):
+class Position(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(20), nullable=False)
-    user = db.relationship("User", back_populates="role")
 
-    @classmethod
-    def get(cls, role_id: int) -> "Role":
-        return cls.query.get(role_id)
-
-    def to_dict(self):
+    def to_dict(self) -> Dict:
         return {"id": self.id, "name": self.name}
