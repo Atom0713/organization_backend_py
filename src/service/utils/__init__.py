b7d4983d09af325c9113d228652a6942bbace401
Artem Sliusarenko <sliusarenko.artem@gmail.com>
2023-03-13 15:25:45 +0100

    date format

--- src/service/utils/__init__.py
+++ src/service/utils/__init__.py
@@ -7,5 +7,6 @@ from .logger import logger
 
 DATE_FORMAT = "%Y-%m-%d"
 DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+EVENT_DATE_FORMAT = "%d %b, %Y %H:%M %p"
 
 __all__: List[str] = ["handle_response", "logger", "ROLES", "HTTPRequestMethods"]
